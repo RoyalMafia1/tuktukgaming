441bd893125ee201825baf0febb4d465f633ddca
RoyalMafia1 <150146643+RoyalMafia1@users.noreply.github.com>
2023-12-21 14:25:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace xXCodemasterzzXx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+
+            string nr = Console.ReadLine();
+            int number = Convert.ToInt32(nr);
+
+            if (number % 2 == 0)
+            {
+                //siia tuleb kutsuda esile meetod, kus on kirjas: sisestasid paaris arvu
+                EvenNumber();
+            }
+            else
+            {
+                //siia tuleb kutsuda esile meetod, kus on kirjas: sisestasid paaritu arvu
+                OddNumber();
+            }
+        }
+
+        static void EvenNumber()
+        {
+            Console.WriteLine("Tegemist on paaris numbriga");
+        }
+
+        static void OddNumber()
+        {
+            Console.WriteLine("Tegemist on paaritu numbriga");
+        }
+    }
+}
+
+   
